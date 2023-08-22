61cafaf516e1d208266cff392ecfee422ca7fa16
anjoo91 <andrew.w.lew@gmail.com>
2023-08-22 12:16:27 -0400

    added assoc_destination functionality & remove assoc_destination functionality via forms.py

--- wanderlust/main_app/forms.py
+++ wanderlust/main_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import Checklist, Activities, User
+from .models import Checklist, Activities, User, Trips, Destinations, Travelers
 
 class ChecklistForm(ModelForm):
   class Meta:
@@ -17,3 +17,9 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+        
+class AddDestinationForm(forms.ModelForm):
+    class Meta:
+        model = Trips
+        exclude = ['name', 'startDate', 'endDate', 'budget', 'user']
+        widgets = {'destination_ids': forms.CheckboxSelectMultiple} 
--- wanderlust/main_app/models.py
+++ wanderlust/main_app/models.py
@@ -2,9 +2,8 @@ from django.db import models
 from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
+
 # Create your models here.
-
-
 class Destinations(models.Model):
     name = models.CharField(max_length=250)
     country = models.CharField(max_length=60)
@@ -12,21 +11,16 @@ class Destinations(models.Model):
     currency = models.CharField(max_length=3)
 
     def __str__(self):
-        return self.name, self.country
+        return self.name
 
     def get_absolute_url(self):
         return reverse('destinations_detail', kwargs={'pk': self.id})
-        
-
-
-
-
 class Trips(models.Model):
     name = models.CharField(max_length=250)
-    startDate = models.DateField()
-    endDate = models.DateField()
-    budget = models.IntegerField()
-    destination_ids = models.ForeignKey(Destinations, on_delete=models.CASCADE, null=True, blank=True)
+    startDate = models.DateField(null=True)
+    endDate = models.DateField(null=True)
+    budget = models.IntegerField(null=True)
+    destination_ids = models.ManyToManyField(Destinations, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -38,26 +32,20 @@ class Trips(models.Model):
     class Meta: 
         ordering = ['-startDate']
 
-
-
-
 class Checklist(models.Model):
     todos = models.CharField(max_length=250)
     complete = models.BooleanField()
     trip_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
-
 class Travelers(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     trip_id = models.ForeignKey(Trips, on_delete=models.CASCADE)
-
 
 class Activities(models.Model):
     plannedAct = models.CharField(max_length=250)
     endDate = models.DateField()
     dueDate = models.DateField()
     trip_id = models.ForeignKey(Trips, on_delete=models.CASCADE)
-
 
 class Photos(models.Model):
     photoUrl = models.CharField(max_length=200)
