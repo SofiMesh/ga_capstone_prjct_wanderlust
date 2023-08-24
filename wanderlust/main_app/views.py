10ef6d9727c396af660b1a3aa8e8627dfcadd72b
SofiMesh <sofimeshvildishvili@gmail.com>
2023-08-24 14:17:16 -0400

    some styling added

--- wanderlust/main_app/views.py
+++ wanderlust/main_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
 
 from .models import Trips, Destinations, Photos, Checklist, Travelers, Activities
 from .forms import ChecklistForm, ActivityForm, SignupForm, AddDestinationForm
